8f0a465b6a694f1895d41eea0ef1a87e3235f78e
abdalla_mmdouh <abdallammdouh6@gmail.com>
2025-05-18 12:05:51 +0300

    add motion to the project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,24 +1,37 @@
+import { motion } from "framer-motion";
 import { MdArrowOutward } from "react-icons/md";
 
 const Project = ({ project }) => {
-    return (
-        <div className="group relative overflow-hidden rounded-3xl">
-            <img
-                className="h-full w-full object-cover transition-transform duration-400 group-hover:scale-110"
-                src={project.image} alt={project.name} />
-            <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0
-        backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100">
-                <h2 className="text-center mb-2 text-xl">{project.name}</h2>
-                <p className="text-center p-2">{project.description}</p>
-                <a href={project.githubLink} className="rounded-full hover:bg-gray-300 bg-white font-medium text-black px-4 py-2">
-                    <div className="flex items-center">
-                        <span>View</span>
-                        <MdArrowOutward />
-                    </div>
-                </a>
-            </div>
-        </div>
-    )
-}
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.9 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.6 }}
+      className="group relative overflow-hidden rounded-3xl"
+    >
+      <img
+        className="h-full w-full object-cover transition-transform duration-400 group-hover:scale-110"
+        src={project.image}
+        alt={project.name}
+      />
+      <div
+        className="absolute inset-0 flex flex-col items-center justify-center opacity-0
+        backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100"
+      >
+        <h2 className="text-center mb-2 text-xl">{project.name}</h2>
+        <p className="text-center p-2">{project.description}</p>
+        <a
+          href={project.githubLink}
+          className="rounded-full hover:bg-gray-300 bg-white font-medium text-black px-4 py-2"
+        >
+          <div className="flex items-center">
+            <span>View</span>
+            <MdArrowOutward />
+          </div>
+        </a>
+      </div>
+    </motion.div>
+  );
+};
 
 export default Project;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,23 @@
+import { motion } from "framer-motion";
 import { PROJECTS } from "../constants";
 import Project from "./Project";
-
 const Projects = () => {
-
-    return (
-        <div className="w-full text-white" id="projects">
-            <h2 className="mb-8 text-center text-3xl lg:text-4xl">Projects</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-3 md:gap-2 gap-1">
-                {
-                    PROJECTS.map((p) => {
-                        return <Project project={p} key={p.id} />
-                    })
-                }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="w-full text-white" id="projects">
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        className="mb-8 text-center font-semibold text-3xl lg:text-4xl"
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        Projects
+      </motion.h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-3 md:gap-2 gap-1">
+        {PROJECTS.map((p) => {
+          return <Project project={p} key={p.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
 export default Projects;
